08ce2a73370266a8574bbb3e52f465516445a03b
王四公子 <wanglin@wangsigongzi.local>
2016-03-02 00:10:59 +0800

    add Php code

---
+++ php_code/getPersonData.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @name 登陆获取用户信息
+ * @author Minato
+ * @since 20160301
+ * @param $_GET['userID'] $_GET['passWord']
+ * @return 1:成功（返回用户的json）, -1:校验失败, -2:参数错误, -3:数据库错误
+ */
+
+require("header.api.php");
+
+$fdb = dbConnect($F_DB); //连接数据库
+
+if (empty($_GET['c_account']) || empty($_GET['c_pass_md5'])) {
+    die("-2");
+}
+
+$c_account = mysql_real_escape_string($_GET['c_account']);
+$c_pass_md5 = mysql_real_escape_string($_GET['c_pass_md5']);
+
+
+$c_id = isExistUser($c_account, $c_pass_md5, $fdb);
+if ($c_id) {
+	getUserData($c_id, DB_KEEP_USER_ACCOUNT, $fdb);
+} else {
+	$c_id = createUser($c_account, $c_pass_md5, DB_KEEP_USER_ACCOUNT, $fdb);
+	getUserData($c_id, DB_KEEP_USER_ACCOUNT, $fdb);
+}
+
+
---
+++ php_code/userFunc.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @name 用户表相关的函数接口
+ * @author Minato
+ * @since 20160301
+ */
+
+//判断是否存在用户
+function isExistUser ($c_account, $c_pass_md5, $fdb)
+{
+	$chkSql = "select c_id from ".DB_KEEP_USER_ACCOUNT." where c_account = '{$c_account}' and c_pass_md5 = '{$c_pass_md5}'";
+	$chkQuery = mysql_query($chkSql, $fdb);
+
+	$result = mysql_fetch_assoc($chkQuery);
+
+	if ($result) {
+		//exist user
+		$c_id = $result['c_id'];
+		return $c_id;
+	}
+	return false;
+}
+
+//创建用户
+function createUser ($c_account, $c_pass_md5, $table_name, $fdb)
+{
+	$exeSql = "insert into ".$table_name." set c_account = '{$c_account}',c_pass_md5 = '{$c_pass_md5}'";
+	$exeQuery = mysql_query($exeSql, $fdb);
+	if(!$exeQuery){
+		die("添加数据失败".mysql_error());
+	}else {
+		return isExistUser($c_account, $c_pass_md5, $fdb);
+	}
+}
+
+//获取用户数据
+function getUserData ($c_id, $table_name, $fdb)
+{
+	$resultSql = mysql_query("select * from ".$table_name." where c_id = {$c_id}", $fdb);
+	while ($row = mysql_fetch_assoc($resultSql)) {
+		$returnData[] = $row;
+	}
+	echo json_encode($returnData);
+}
+
+
+
